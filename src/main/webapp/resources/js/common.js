591fe568b6553f98e1f2eb49a56650bb92413c8f
hjy <heojungyeon.97@gmail.com>
2022-05-31 21:08:36 +0900

    * 코드 정리

--- src/main/webapp/resources/js/common.js
+++ src/main/webapp/resources/js/common.js
@@ -1,9 +1,13 @@
-// 영화관 찾기 메뉴
-var findCinemaMenuClose = false;
+/* 
+ * 헤더 영화관찾기 메뉴
+ * 배너 
+ */ 
+
+var findCinemaMenuClose = false
 
 function findCinemaMenu() {
 	if (!findCinemaMenuClose) {
-		$('.cinema').css('display', 'block');
+		$('.cinema').css('display', 'block')
 		findCinemaMenuClose = true;
 	} else {
 		closeCinemaMenu()
@@ -12,89 +16,87 @@ function findCinemaMenu() {
 
 function findCinemaMobileMenu() {
 	toggleMenu()
-	$('.cinema').css('display', 'block');
+	$('.cinema').css('display', 'block')
 }
 
+// 헤더 영화관 찾기 메뉴 - 닫기 (닫으면 '전체'로 표시되게 설정)
 function closeCinemaMenu() {
-	$('.cinema').css('display', 'none');
-	findCinemaMenuClose = false;
-	$('.cinema-address-item').each(function(){
-		if ($(this).hasClass('on') == 1) $(this).removeClass('on');
-	});
-	$('.cinema-address-item').first().addClass('on');
+	$('.cinema').css('display', 'none')
+	findCinemaMenuClose = false
+	$('.cinema-address-item').first().addClass('on').siblings().removeClass('on')
 	$('.cinema-search-item').val('')
 	searchCinema('address', '')
 }
 
-$(document).on("propertychange change keyup paste input", ".cinema-search-item", function(){ 
-	var target = 'cinema_name';
-	var search = $.trim($('.cinema-search-item').val())
+// 헤더 영화관 찾기 메뉴 - 검색
+$(document).on("propertychange change keyup paste input", ".cinema-search-item", function() { 
+	var target = 'cinema_name'
+	var search = $('.cinema-search-item').val().trim()
 	searchCinema(target, search)
-});
+})
 	
+// 헤더 영화관 찾기 메뉴 - 지역 클릭
 function searchAddress(target, search, num) {
-	$('.cinema-address-item').each(function(){
-		if ($(this).hasClass('on') == 1) $(this).removeClass('on');
-	});
-	$('.cinema-address-item').eq(num).addClass('on');
+	$('.cinema-address-item').eq(num).addClass('on').siblings().removeClass('on')
 	searchCinema(target, search)
 }
 
+//헤더 영화관 찾기 메뉴 - 검색과 지역 클릭 결과
 function searchCinema(target, search) {
     $.ajax ({
         method: 'GET',
         url: 'cinema_search_menu',
         data: {"target": target, "search": search},
         success: function(dataList) {
-        	$('.cinema-local-result').html('');
-        	var addTag = "";
+        	$('.cinema-local-result').html('')
+        	var addTag = '';
         	$.each(dataList, function(index, data){
-        		addTag += "<a class='cinema-local-item' href='cinema_detail?no=" + data['cinema_id'] +"'>" + data['cinema_name'] +"</a>";
+        		addTag += "<a class='cinema-local-item' href='cinema_detail?no=" + data['cinema_id'] +"'>" + data['cinema_name'] +"</a>"
             })
-            $('.cinema-local-result').append(addTag); 
+            $('.cinema-local-result').append(addTag)
         }
     })
 }
 
 // main banner - 2초마다 3개의 이미지 변경
 function changeBanner() {
-    change($('#bannerIndex').val());
-    var num = $('#bannerIndex').val();
+    change($('#bannerIndex').val())
+    var num = $('#bannerIndex').val()
     num++;
-    document.getElementById('bannerIndex').setAttribute('value', num);
+    document.getElementById('bannerIndex').setAttribute('value', num)
     first_change();
 }
 
 function change(val) {
-    $('.img_box').eq(val).siblings().filter('.img_box').hide();
-    $('a', '.a_box').eq(val).siblings().filter('a').hide();
-    $('.img_box').eq(val).show();
-    $('a', '.a_box').eq(val).show();
-    $('li', '.label').eq(val).css('border', ' 2px solid #4285f4');
-    $('li', '.label').eq(val).siblings().filter('li').css('border', '');
+    $('.img_box').eq(val).siblings().filter('.img_box').hide()
+    $('a', '.a_box').eq(val).siblings().filter('a').hide()
+    $('.img_box').eq(val).show()
+    $('a', '.a_box').eq(val).show()
+    $('li', '.label').eq(val).css('border', ' 2px solid #4285f4')
+    $('li', '.label').eq(val).siblings().filter('li').css('border', '')
 }
 
 function first_change() {
     setInterval(function() {
-    if(document.getElementById('find').value == 1){}
-    else{
-        change($('#bannerIndex').val());
+    if (document.getElementById('find').value == 1) {}
+    else {
+        change($('#bannerIndex').val())
         var num = $('#bannerIndex').val()
         if (num == 2)
-            document.getElementById('bannerIndex').setAttribute('value', 0);
+            document.getElementById('bannerIndex').setAttribute('value', 0)
         else {
             num++;
-            document.getElementById('bannerIndex').setAttribute('value', num);
+            document.getElementById('bannerIndex').setAttribute('value', num)
         }
     }
-    }, 2000);
+    }, 2000)
 }
 
 $('li','.label').mouseover(function(){
-    document.getElementById('find').setAttribute('value', 1);
+    document.getElementById('find').setAttribute('value', 1)
     }).mouseout(function(){
-    document.getElementById('find').setAttribute('value', 0);
-});
+    document.getElementById('find').setAttribute('value', 0)
+})
 
 // mobile side menu
 function toggleMenu() {
@@ -103,8 +105,8 @@ function toggleMenu() {
   navbar.toggleClass('side-open')
 
   if (navbar.hasClass('side-open')) {
-    $('body').css('overflow-y', 'hidden');
+    $('body').css('overflow-y', 'hidden')
   } else {
-    $('body').css('overflow-y', 'auto');
+    $('body').css('overflow-y', 'auto')
   }
 }
